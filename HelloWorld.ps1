50ea4940c3320e4beae84a22d8e5412cffa1ac6f
anjula33 <adahanayake3@gmail.com>
2023-09-28 01:05:59 +0530

    added second script

--- HelloWorld.ps1
+++ HelloWorld.ps1
@@ -3,3 +3,9 @@ $name = $env:USERNAME
 Write-host "Hello world!!, Your name is $Anjula, Today is $Wednesday"
 
 #This is a comment to my new powershell script
+
+$date = get-date -format MMMM/MM/YYYY
+$time = get-date -format hh:mm:tt
+Write-Host “Today is $date”, time is “$time”
+Write-Host “Hello $env:COMPUTERNAME, This is your first powershel script"
+Write-Host "Added update to the repo"
